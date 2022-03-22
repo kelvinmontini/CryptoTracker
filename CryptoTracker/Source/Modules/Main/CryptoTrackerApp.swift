9c4fad577f809dc6ce0c1f72beba9fcc09caa0cf
Kelvin Montini <kelvin.montini@gmail.com>
2022-03-21 23:39:26 -0300

    Initial project setup

---
+++ CryptoTracker/Source/Modules/Main/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Kelvin Montini on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ CryptoTracker/Source/Shared/Extensions/Color+Extension.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftUI
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
+
+extension Color {
+    static let theme = ColorTheme()
+}
