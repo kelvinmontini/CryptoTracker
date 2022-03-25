3288314e89668e0d0041a0838c79b5bf4ad8ff2b
Kelvin Montini <kelvin.montini@gmail.com>
2022-03-25 00:05:08 -0300

    add a sketch of the services layer

--- CryptoTracker/Source/Modules/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Source/Modules/Home/ViewModels/HomeViewModel.swift
@@ -1,13 +1,24 @@
 import Foundation
+import Combine
 
 final class HomeViewModel: ObservableObject {
+
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
 
+    private let service = HomeService()
+    private var cancellables = Set<AnyCancellable>()
+
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+
+    func addSubscribers() {
+
+        service.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
