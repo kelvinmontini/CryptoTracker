9a7c8754566c555f9f1ede0b8444075386d9b1f5
Kelvin Montini <kelvin.montini@gmail.com>
2022-03-29 00:47:37 -0300

    add tap gesture in HomeView to close keyboard

--- CryptoTracker/Source/Modules/Home/Views/HomeView.swift
+++ CryptoTracker/Source/Modules/Home/Views/HomeView.swift
@@ -32,6 +32,9 @@ struct HomeView: View {
                 Spacer(minLength: 0)
             }
         }
+        .onTapGesture {
+            UIApplication.shared.endEditing()
+        }
     }
 }
 
